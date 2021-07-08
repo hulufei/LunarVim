4fa0e0d5d698a52c11edead5c7a29acc407c93e9
hulufei <ihulufei@icloud.com>
2021-07-08 16:13:35 +0800

    Fix rust lsp performance issue

--- lua/default-config.lua
+++ lua/default-config.lua
@@ -191,6 +191,7 @@ O = {
       filetypes = { "rb", "erb", "rakefile", "ruby" },
     },
     rust = {
+      active = false,
       rust_tools = {
         active = false,
         parameter_hints_prefix = "<-",
